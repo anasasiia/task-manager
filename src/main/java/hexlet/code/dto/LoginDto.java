72b014ca5d643f298ab80d2202e5d95758c50762
anasasiia <sheldoncooperbuga@gmail.com>
2023-04-05 12:14:00 +0400

    fixed loginDto

--- src/main/java/hexlet/code/dto/LoginDto.java
+++ src/main/java/hexlet/code/dto/LoginDto.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginDto {
-    private String firstname;
+    private String firstName;
     private String lastName;
     private String email;
     private String password;
